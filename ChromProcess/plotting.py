843fdcb3ca9cf0d7de848d95a736ba9a271fe37e
W_E_Robinson <wills_email>
2021-04-21 09:53:15 +0200

    Confidence ellipse code update.

--- ChromProcess/plotting.py
+++ ChromProcess/plotting.py
@@ -848,18 +848,18 @@ def confidence_ellipse(x, y, ax = None, weights = None, n_std=3.0,
     from matplotlib.patches import Ellipse
     import matplotlib.transforms as transforms
     import numpy as np
+    from ChromProcess import plotting
 
     if x.size != y.size:
         raise ValueError("x and y must be the same size")
 
     cov = np.cov(x, y, aweights = weights)
-    pearson = cov[0, 1]/np.sqrt(cov[0, 0] * cov[1, 1])
-    # Using a special case to obtain the eigenvalues of this
-    # two-dimensionl dataset.
-    ell_radius_x = np.sqrt(1 + pearson)
-    ell_radius_y = np.sqrt(1 - pearson)
-    ellipse = Ellipse((0, 0), width=ell_radius_x * 2, height=ell_radius_y * 2,
-                      facecolor=facecolor, edgecolor = edgecolor, **kwargs)
+
+    ellipse = plotting.create_ellipse((0, 0), cov,
+                              facecolor = facecolor,
+                              edgecolor = edgecolor,
+                              **kwargs)
+
 
     # Calculating the stdandard deviation of x from
     # the squareroot of the variance and multiplying
@@ -878,3 +878,72 @@ def confidence_ellipse(x, y, ax = None, weights = None, n_std=3.0,
 
     ellipse.set_transform(transf + ax.transData)
     return ax.add_patch(ellipse)
+
+def confidence_ellipse_from_mean_cov(mean, cov, ax = None, weights = None, n_std=3.0,
+                        facecolor='none', edgecolor = 'k',**kwargs):
+    """
+    Adapted from matplotlib documentation.
+
+    Create a plot of the covariance confidence ellipse of *x* and *y*.
+
+    Parameters
+    ----------
+    x, y : array-like, shape (n, )
+        Input data.
+
+    ax : matplotlib.axes.Axes
+        The axes object to draw the ellipse into.
+
+    n_std : float
+        The number of standard deviations to determine the ellipse's radiuses.
+
+    **kwargs
+        Forwarded to `~matplotlib.patches.Ellipse`
+
+    Returns
+    -------
+    matplotlib.patches.Ellipse
+    """
+    from matplotlib.patches import Ellipse
+    import matplotlib.transforms as transforms
+    import numpy as np
+    from ChromProcess import plotting
+
+    ellipse = plotting.create_ellipse((0, 0), cov,
+                              facecolor = facecolor,
+                              edgecolor = edgecolor,
+                              **kwargs)
+
+
+    # Calculating the stdandard deviation of x from
+    # the squareroot of the variance and multiplying
+    # with the given number of standard deviations.
+    scale_x = np.sqrt(cov[0, 0]) * n_std
+    mean_x = mean[0]
+
+    # calculating the stdandard deviation of y ...
+    scale_y = np.sqrt(cov[1, 1]) * n_std
+    mean_y = mean[1]
+
+    transf = transforms.Affine2D() \
+        .rotate_deg(45) \
+        .scale(scale_x, scale_y) \
+        .translate(mean_x, mean_y)
+
+    ellipse.set_transform(transf + ax.transData)
+    return ax.add_patch(ellipse)
+
+
+def create_ellipse(mean, cov,
+                  facecolor = 'k',edgecolor = 'k', **kwargs):
+
+    from matplotlib.patches import Ellipse
+
+    pearson = cov[0, 1]/np.sqrt(cov[0, 0] * cov[1, 1])
+    ell_radius_x = np.sqrt(1 + pearson)
+    ell_radius_y = np.sqrt(1 - pearson)
+
+    ellipse = Ellipse(mean, width=ell_radius_x * 2, height=ell_radius_y * 2,
+                      facecolor=facecolor, edgecolor = edgecolor, **kwargs)
+
+    return ellipse
