1ba89c8840f316d0a02bb8fc1a4d0198ee9931cb
Will <werobinson6@gmail.com>
2021-08-05 17:41:46 +0200

    moved function over. emptied one file.

--- ChromProcess/calibration/calibration_operations.py
+++ ChromProcess/calibration/calibration_operations.py
@@ -1,3 +1,30 @@
+def CreateCalibrationSeries(series):
+
+    '''
+    Replace sample numbers with concentrations.
+
+    Parameters
+    -----------
+
+    Returns
+    -------
+
+    '''
+    stock_conc = (series.conditions["Mass/ g"][0]/ series.conditions["Mr/ g/mol"][0])/series.conditions["Stock_vol/ L"][0]
+
+    mol_series = [stock_conc*x for x in series.conditions["Titration_series/ L"]]
+
+    series_conc = [(x/series.conditions["Sample_volume/ L"][0])/series.internal_ref_concentration for x in mol_series]
+
+    series.x_series = series_conc
+
+    series.x_name = "[sample]/[internal standard]"
+
+    series.conditions["internal_ref_concentration/ M"] = [series.internal_ref_concentration]
+
+    for c,v in enumerate(series.chromatograms):
+        v.timepoint = series.x_series[c]
+
 def calibration_series_values(exp_conds, analysis):
     import numpy as np
 
