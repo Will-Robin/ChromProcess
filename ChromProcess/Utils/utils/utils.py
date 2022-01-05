b6ba537d6bad97c61af985af817546b478d81b11
Will <werobinson6@gmail.com>
2022-01-05 14:47:28 +0100

    add function to utils

--- ChromProcess/Utils/utils/utils.py
+++ ChromProcess/Utils/utils/utils.py
@@ -91,9 +91,41 @@ def bin_dictionary(value_dict, stdev = 0.001):
                 else:
                     for count,p in enumerate(out_log[position]):
                         if p == 0:
-                            out_log[position][count] = value_dict[values][count]
+                            out_log[position][count] = value_dict[value][count]
                         else:
                             pass
 
     return out_log
 
+def peak_dict_to_spreadsheet(peak_dict, series_values, series_unit):
+    '''
+    Convert a dictionary of peak series to a spreadsheet-like grid and a
+    header.
+
+    Parameters
+    ----------
+    peak_dict: dict
+        Dictionary of peak series.
+    series_values: list
+        list of series values
+
+    Returns
+    -------
+    peak_header, peak_grid_transposed: (list, list of lists)
+    '''
+
+    peak_grid = []
+    peak_header = [series_unit]
+
+    peak_grid.append(series_values)
+
+    # writing data
+    sorted_keys = sorted([*peak_dict], key = lambda x:x.count('C'))
+    
+    for s in sorted_keys:
+        peak_header.append(s)
+        peak_grid.append(peak_dict[s])
+
+    peak_grid_transposed = [list(i) for i in zip(*peak_grid)]
+
+    return peak_header, peak_grid_transposed
