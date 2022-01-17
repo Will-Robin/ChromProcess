df1dd2dbeeba269a4cd762cf33f9cd69e12b3de0
W_E_Robinson <wills_email>
2022-01-17 15:44:35 +0100

    refactor ion chrom from peak and return result

--- ChromProcess/Loading/chromatogram/ion_chromatogram/ion_chromatogram_from_peak.py
+++ ChromProcess/Loading/chromatogram/ion_chromatogram/ion_chromatogram_from_peak.py
@@ -1,75 +1,46 @@
 import numpy as np
-from ChromProcess.Utils.utils import utils as ut
+from .ion_chromatogram_from_region import ion_chromatogram_from_region
 
 def ion_chromatogram_from_peak(
                                 peak, 
                                 parent_chromatogram,
-                                ic_filter = 0.05, 
-                                spectrum_filter = 0.1
+                                spectrum_filter = 0.1,
+                                threshold = 0.1
                                 ):
     '''
+    Create a dictionary of ion chromatograms using information from the peak
+    and its parent chromatogram.
+
+    Parameters
+    ----------
     peak: ChromProcess Peak object
+        Peak containing information.
     parent_chromatogram: ChromProcess Chromatogram object
-    ic_filter: float
-        ion chromatograms which do not exceed this fraction of the
-        maximum total ion chromatogram peak height will not be included.
+       Chromatogram containing information.
     spectrum_filter: float
         m/z intensities which do not exceed this fraction its parent
         mass spectrum will be omitted from the ion chromatogram.
+    threshold: float
+        Threshold for mass spectra extraction relative to the maximum signal
+        in the region.
 
-    modifies peak.ion_chromatograms dictionary in Peak object
+    Returns
+    -------
+    ion_chromatograms: dict[mz]:array()
+        A dictionary of ion chromatograms keyed by m/z value.
     '''
-
-    # wipe previously stored ion chromatograms
-    peak.ion_chromatograms = {}
 
     # Get the indices of the peaks data points in the chromatogram
     inds = peak.indices
 
     # find the relevant chromatogram attribute sections
     time = parent_chromatogram.time[inds]
-    signal = parent_chromatogram.signal[inds]
-    scan_inds = parent_chromatogram.scan_indices[inds]
-    p_counts = parent_chromatogram.point_counts[inds]
 
-    # iterate over mass spectra recorded at each time point
-    for s in range(0,len(time)):
-        # get mass spectrum at time point
-        inten = parent_chromatogram.mz_intensity[scan_inds[s]:scan_inds[s]+p_counts[s]]
-        mz_values = parent_chromatogram.mz_values[scan_inds[s]:scan_inds[s]+p_counts[s]]
+    ion_chromatograms = ion_chromatogram_from_region(
+                                                    parent_chromatogram,
+                                                    time.min(), 
+                                                    time.max(), 
+                                                    spectrum_filter = spectrum_filter, 
+                                                    threshold = threshold)
 
-        # filter out low intensity m/z signals
-        filt_inds = np.where(inten > spectrum_filter*np.amax(inten))[0]
-
-        if len(filt_inds) > 0:
-
-            inten = inten[filt_inds]
-            mz_values = mz_values[filt_inds]
-
-            # round m/z to 2 d.p.
-            rounded_mz = np.round(mz_values, 2)
-
-            # add the intensity values into the appropriate m/z channel
-            for m in range(0,len(rounded_mz)):
-                if rounded_mz[m] in peak.ion_chromatograms:
-                    peak.ion_chromatograms[rounded_mz[m]][s] = inten[m]
-                else:
-                    peak.ion_chromatograms[rounded_mz[m]] = np.zeros(len(time))
-                    peak.ion_chromatograms[rounded_mz[m]][s] = inten[m]
-
-        else:
-            pass
-
-    # combine channels with close enough m/z value (given stdev)
-    ut.bin_dictionary(peak, stdev = 0.1)
-
-    # remove ion chromatograms whose maximum values do not exceed the
-    # fraction of the total ion chormatogram defind by ic_filter
-    threshold = ic_filter*signal.max()
-    remove_ic = []
-    for ic in peak.ion_chromatograms:
-        if peak.ion_chromatograms[ic].max() < threshold:
-            remove_ic.append(ic)
-
-    for i in remove_ic:
-        del peak.ion_chromatograms[i]
+    return ion_chromatograms
--- ChromProcess/Loading/chromatogram/ion_chromatogram/ion_chromatogram_from_region.py
+++ ChromProcess/Loading/chromatogram/ion_chromatogram/ion_chromatogram_from_region.py
@@ -1,6 +1,13 @@
 import numpy as np
+from ChromProcess.Utils.utils import utils
 
-def ion_chromatogram_from_region(chromatogram, lower, upper, threshold = 0.1):
+def ion_chromatogram_from_region(
+                                chromatogram, 
+                                lower, 
+                                upper, 
+                                spectrum_filter = 0.1,
+                                threshold = 0.1
+                                ):
     '''
     Get the ion chromatograms for a region of a chromatogram. Requires mass
     spectra information to be present in the chromatogram.
@@ -13,50 +20,71 @@ def ion_chromatogram_from_region(chromatogram, lower, upper, threshold = 0.1):
         Lower bound for retention time region in chromatogram
     upper: float
         Upper bound for retention time region in chromatogram
-
+    spectrum_filter: float
+        m/z intensities which do not exceed this fraction its parent
+        mass spectrum will be omitted from the ion chromatogram.
     threshold: float
         Threshold for mass spectra extraction relative to the maximum signal
         in the region.
 
     Returns
     -------
-
-    ion_dict: dict
+    ion_chromatograms: dict
         dictionary of ion chromatograms
         {m/z: intensities over time}
     '''
 
-    if not chromatogram.mass_spectra:
+    if len(chromatogram.mz_values) == 0:
         return None
     else:
-        ion_dict = {}
-        inds = np.where((chromatogram.time > lower)&(chromatogram.time < upper))[0]
+        inds = utils.indices_from_boundary(chromatogram.time, lower, upper)
 
         time = chromatogram.time[inds]
+        signal = chromatogram.signal[inds]
         scan_inds = chromatogram.scan_indices[inds]
         p_counts = chromatogram.point_counts[inds]
 
-        for s in range(0,len(time)):
+        mz_regions = {}
 
+        # iterate over mass spectra recorded at each time point
+        for s in range(0,len(time)):
+            # get mass spectrum at time point
             inten = chromatogram.mz_intensity[scan_inds[s]:scan_inds[s]+p_counts[s]]
             mz_values = chromatogram.mz_values[scan_inds[s]:scan_inds[s]+p_counts[s]]
 
-            if len(inten) > 0:
+            # filter out low intensity m/z signals
+            filt_inds = np.where(inten > threshold*np.amax(inten))[0]
 
-                filt_inds = np.where(inten > threshold*np.amax(inten))[0]
+            if len(filt_inds) > 0:
+
                 inten = inten[filt_inds]
                 masses = mz_values[filt_inds]
 
                 round = np.round(masses, 2)
 
+                # add the intensity values into the appropriate m/z channel
                 for m in range(0,len(round)):
-                    if round[m] in ion_dict:
-                        ion_dict[round[m]][s] = inten[m]
+                    if round[m] in mz_regions:
+                        mz_regions[round[m]][s] = inten[m]
                     else:
-                        ion_dict[round[m]] = np.zeros(len(time))
-                        ion_dict[round[m]][s] = inten[m]
+                        mz_regions[round[m]] = np.zeros(len(time))
+                        mz_regions[round[m]][s] = inten[m]
             else:
                 pass
 
-    return ion_dict
+    # combine channels with close enough m/z value (given stdev)
+    ion_chromatograms = utils.bin_dictionary(mz_regions, stdev = 0.1)
+
+    # remove ion chromatograms whose maximum values do not exceed the
+    # fraction of the total ion chromatogram defind by threshold
+    threshold = threshold*signal.max()
+    remove_ic = []
+    for ic in ion_chromatograms:
+        if ion_chromatograms[ic].max() < threshold:
+            remove_ic.append(ic)
+
+    for i in remove_ic:
+        del ion_chromatograms[i]
+
+    return ion_chromatograms
 
