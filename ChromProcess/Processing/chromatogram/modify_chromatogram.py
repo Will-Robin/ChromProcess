ee9622f855f8e1e4f78d0467bb4e36c3025360a0
W_E_Robinson <wills_email>
2022-02-02 16:40:30 +0100

    update docstrings

--- ChromProcess/Processing/chromatogram/modify_chromatogram.py
+++ ChromProcess/Processing/chromatogram/modify_chromatogram.py
@@ -4,6 +4,8 @@ from ChromProcess.Processing.chromatogram import find_peaks
 
 def add_peaks_to_chromatogram(peak_times, chromatogram):
     '''
+    Add peaks to a chromatogram (modifies the chromatogram in place).
+
     Parameters
     ----------
     peak_times: list
@@ -28,10 +30,14 @@ def add_peaks_to_chromatogram(peak_times, chromatogram):
 
 def integrate_chromatogram_peaks(chromatogram, baseline_subtract = False):
     '''
+    Integrate all of the peaks in a chromatogram (modifies them in place).
+
     Parameters
     ----------
-
-    chromatogram: Chromatogram object
+    chromatogram: Classes.Chromatogram object
+        Chromatogram containing peaks.
+    baseline_subtract: bool
+        Whether to perform a local baseline subtraction on the peak.
 
     Returns
     ------
@@ -53,6 +59,12 @@ def internal_standard_integral(chromatogram, is_start, is_end):
     series: Chromatogram_Series object
         Object containing chromatograms and associated series data which is
         modified by the function.
+    is_start: float
+        Start of the region of the chromatogram in which the internal standard
+        is found.
+    is_end: float
+        End of the region of the chromatogram in which the internal standard
+        is found.
 
     Returns
     ------
