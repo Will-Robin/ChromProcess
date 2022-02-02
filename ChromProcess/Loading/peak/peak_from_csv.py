b4eb9e9291b64f504d154fd8fea1dc9005121a1b
W_E_Robinson <wills_email>
2022-02-02 14:58:49 +0100

    update annotations, format code cosmetically and use simpler method for finding the nearest retention time to a give time value.

--- ChromProcess/Loading/peak/peak_from_csv.py
+++ ChromProcess/Loading/peak/peak_from_csv.py
@@ -1,58 +1,86 @@
 import numpy as np
-
+from ChromProcess.Utils.utils.utils import peak_indices_to_times
+from ChromProcess.Utils.peak_finding.pick_peaks import find_peak_boundaries_look_ahead
 from ChromProcess.Loading.peak_collection.peak_collection_from_csv import peak_collection_from_csv
 
-
-def peak_rt_from_file(chromatogram, peak_file):
+def peak_rt_from_file(peak_file, chromatogram):
     '''
     Get peak retention time indices from a PeakCollections file.
-    chromatogram: Chromatogram object
-        Chromatogram object to which the peaks should be added
+
+    Parameters
+    ----------
+    chromatogram: Classes.Chromatogram object
+        Chromatogram object to which the peaks should be added.
     peak_file: string
         Location of peak collection csv
-    
+
     Returns
     -------
     peak_indices: ndarray  
         array of peak indices
     '''
+
     peak_collection = peak_collection_from_csv(peak_file,round_digits=7)
     peak_retention_times = [p.retention_time for p in peak_collection.peaks]
 
-    peaks_indices = np.empty(0,dtype='int64') #the functions written to find the start and end of the peak relies on indexes, so we need to convert the retention times to their corresponding indexes.
+    # Find the indices of the retention times in the 
+    # chromatogram time axis.
+    peaks_indices = np.empty(0,dtype='int64') 
     for p_rt in peak_retention_times:
-        idx = np.where(chromatogram.time == float(p_rt)) #This covers any exact matches
-        if len(idx[0]) == 0: #no exact match found, this should mean that a value was manually inserted, the function will now search the closest time value.
-            search_idx = np.searchsorted(chromatogram.time, float(p_rt), "left")
-            max_signal = np.max(chromatogram.signal[search_idx-4:search_idx+4]) #search the 9 nearest value for the highest peak, this gives a bit of play for time input
-            idx = np.where(chromatogram.signal == max_signal)
-        peaks_indices = np.append(peaks_indices, int(idx[0]))
+        # This covers any exact matches
+        idx = np.where(chromatogram.time == p_rt)[0]
+
+        if len(idx) == 0: 
+            # If the exact value of p_rt is not in the time axis, 
+            # find the closest time value.
+            delta_rt = np.abs(chromatogram.signal - p_rt)
+            # Create in list; similar behaviour to return of np.where()[0]
+            idx = [delta_rt.argmin()]
+
+        peaks_indices = np.append(peaks_indices, idx[0])
+
     return peaks_indices
 
-
-def peak_from_csv(chromatogram, peak_file, look_ahead = 12):
+def peak_from_csv(peak_file, chromatogram, look_ahead = 12):
     '''
-    Get the peak retention times from a PeakCollections file rather than directly from a chromatogram.
-    Peak boundaries will be generated. 
+    Get the peak retention times from a PeakCollections file rather 
+    than directly from a chromatogram.
 
-    chromatogram: Chromatogram object
+    Parameters
+    ----------
+    chromatogram: Classes.Chromatogram object
         Chromatogram object to which the peaks should be added
     peak_file: string
         Location of peak collection csv
     peak_window: int
-        Number of spaces the function will search through to find the start or end of a peak
+        Number of spaces the function will search through to 
+        find the start or end of a peak
 
     Returns
     -------
     peak_features: list
         list of list containing times of peak start, center, and end.
     '''
-    from ChromProcess.Utils.peak_finding.pick_peaks import find_peak_boundaries_look_ahead
-    from ChromProcess.Utils.utils.utils import peak_indices_to_times
-    
-    peaks_indices =  peak_rt_from_file(chromatogram, peak_file)
-    peak_starts, peak_ends = find_peak_boundaries_look_ahead(chromatogram.signal, peaks_indices, look_ahead=1)
-    picked_peaks = {'Peak_indices':peaks_indices, 'Peak_start_indices':peak_starts, 'Peak_end_indices':peak_ends}
-    peak_features = peak_indices_to_times(chromatogram.time, picked_peaks)
-    
+
+    peaks_indices = peak_rt_from_file(
+                                    peak_file,
+                                    chromatogram
+                                    )
+
+    peak_starts, peak_ends = find_peak_boundaries_look_ahead(
+                                                            chromatogram.signal,
+                                                            peaks_indices,
+                                                            look_ahead = 1
+                                                            )
+    picked_peaks = {
+                    'Peak_indices': peaks_indices,
+                    'Peak_start_indices': peak_starts,
+                    'Peak_end_indices': peak_ends
+                    }
+
+    peak_features = peak_indices_to_times(
+                                        chromatogram.time,
+                                        picked_peaks
+                                        )
+
     return peak_features
