0b2b7be4a66f1fbe5dcc64025af8866ec249e34f
Will <werobinson6@gmail.com>
2022-02-06 10:36:27 +0100

    Refactor data report csv writer to place text writing in a separate function.

--- ChromProcess/Writers/data_report/data_report_to_csv.py
+++ ChromProcess/Writers/data_report/data_report_to_csv.py
@@ -3,8 +3,79 @@ from pathlib import Path
 from ChromProcess.Utils.utils import utils
 from ChromProcess.Writers.general import write_header
 
+def write_data_report_text(data_report):
+    '''
+    Write a formatted text version of the data report.
+
+    Parameters
+    ----------
+    data_report: Classes.DataReport
+        DataReport to convert to text.
+
+    Returns
+    -------
+    data_report_text: str
+        Text output for the data report.
+    '''
+
+    header_text = write_header.write_conditions_header(
+                                            data_report.name,
+                                            data_report.conditions,
+                                            data_report.analysis
+                                            )
+
+    conc_header, conc_grid = utils.peak_dict_to_spreadsheet(
+                                        data_report.data,
+                                        data_report.series_values,
+                                        data_report.series_unit
+                                        )
+
+    peak_err_header, err_grid = utils.peak_dict_to_spreadsheet(
+                                        data_report.errors,
+                                        data_report.series_values,
+                                        data_report.series_unit
+                                        )
+
+    data_report_text = ''
+
+    data_report_text += header_text
+
+    data_report_text += "start_data\n"
+
+    for x in conc_header:
+        data_report_text += f"{x},"
+
+    data_report_text += "\n"
+
+    for x in range(0,len(conc_grid)):
+        for y in range(0,len(conc_grid[x])):
+            val = conc_grid[x][y]
+            data_report_text += f"{val},"
+        data_report_text += "\n"
+
+    data_report_text += "end_data\n"
+
+    data_report_text += "start_errors\n"
+
+    for x in peak_err_header:
+        data_report_text += f"{x},"
+
+    data_report_text += "\n"
+
+    for x in range(0,len(err_grid)):
+        for y in range(0,len(err_grid[x])):
+            val = err_grid[x][y]
+            data_report_text += f"{val},"
+        data_report_text += "\n"
+
+    data_report_text += "end_errors\n"
+
+    return data_report_text
+
 def data_report_to_csv(data_report, filename = '', path = None):
     '''
+    Write a data report to a csv file.
+
     Parameters
     ----------
     data_report: Classes.DataReport
@@ -13,6 +84,10 @@ def data_report_to_csv(data_report, filename = '', path = None):
         name for file
     path: pathlib Path object
         Path to folder for file storage.
+
+    Returns
+    -------
+    None
     '''
 
     if isinstance(path, str):
@@ -26,51 +101,8 @@ def data_report_to_csv(data_report, filename = '', path = None):
     else:
         fname = path/filename
 
-    header_text = write_header.write_conditions_header(
-                                            data_report.name, 
-                                            data_report.conditions, 
-                                            data_report.analysis
-                                            )
-    conc_header, conc_grid = utils.peak_dict_to_spreadsheet(
-                                        data_report.data, 
-                                        data_report.series_values,
-                                        data_report.series_unit
-                                        )
-    peak_err_header, err_grid = utils.peak_dict_to_spreadsheet(
-                                        data_report.errors, 
-                                        data_report.series_values,
-                                        data_report.series_unit
-                                        )
+    data_report_as_string = write_data_report_text(data_report)
 
-    with open(fname, 'w') as outfile:
-
-        outfile.write(header_text)
-
-        outfile.write("start_data\n")
-
-        [outfile.write("{},".format(x)) for x in conc_header]
-
-        outfile.write("\n")
-
-        for x in range(0,len(conc_grid)):
-            for y in range(0,len(conc_grid[x])):
-                val = conc_grid[x][y]
-                outfile.write(f"{val},")
-            outfile.write("\n")
-
-        outfile.write("end_data\n")
-
-        outfile.write("start_errors\n")
-
-        [outfile.write("{},".format(x)) for x in peak_err_header]
-
-        outfile.write("\n")
-
-        for x in range(0,len(err_grid)):
-            for y in range(0,len(err_grid[x])):
-                val = err_grid[x][y]
-                outfile.write(f"{val},")
-            outfile.write("\n")
-
-        outfile.write("end_errors\n")
+    with open(fname, 'w') as file:
+        file.write(data_report_as_string)
 
