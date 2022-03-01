69a4e623c9e713a70242b9dd186470a332a0930f
W_E_Robinson <wills_email>
2022-03-01 16:12:24 +0100

    update write module to separate out text writing from file writing

--- ChromProcess/Writers/peak_collection/peak_collection_to_csv.py
+++ ChromProcess/Writers/peak_collection/peak_collection_to_csv.py
@@ -1,5 +1,47 @@
 from pathlib import Path
 
+def write_peak_collection_text(peak_collection):
+    """
+    Write a PeakCollection to a csv string.
+
+    Parameters
+    ----------
+    peak_collection: Classes.PeakCollection
+    directory: str or pathlib Path
+
+    Returns
+    -------
+    peak_collection_text: string
+    """
+
+    unit = peak_collection.series_unit
+    value = peak_collection.series_value
+
+    IS_rt = peak_collection.internal_standard.retention_time
+    IS_integ = peak_collection.internal_standard.integral
+    strt = peak_collection.internal_standard.start
+    end = peak_collection.internal_standard.end
+
+    peak_collection_text = f"{unit},{value}\n"
+    peak_collection_text += "IS_retention_time/ min,"
+    peak_collection_text += "IS_integral,"
+    peak_collection_text += "IS_peak start/ min,"
+    peak_collection_text += "IS_peak end/ min\n"
+    peak_collection_text += f"{IS_rt},{IS_integ},{strt},{end}\n"
+
+    peak_collection_text += "Retention_time/ min,"
+    peak_collection_text += "integral,"
+    peak_collection_text += "peak start/ min,"
+    peak_collection_text += "peak end/ min\n"
+
+    for p in peak_collection.peaks:
+        rt = p.retention_time
+        integ = p.integral
+        start = p.start
+        end = p.end
+        peak_collection_text += f"{rt},{integ},{start},{end}\n"
+
+    return peak_collection_text
 
 def peak_collection_to_csv(peak_collection, directory=""):
     """
@@ -22,34 +64,8 @@ def peak_collection_to_csv(peak_collection, directory=""):
     elif isinstance(directory, Path):
         fname = directory / f"{peak_collection.filename}"
 
-    IS_header = ""
-    IS_header += "IS_retention_time/ min,"
-    IS_header += "IS_integral,"
-    IS_header += "IS_peak start/ min,"
-    IS_header += "IS_peak end/ min\n"
+    text = write_peak_collection_text(peak_collection)
 
-    pk_header = ""
-    pk_header += "Retention_time/ min,"
-    pk_header += "integral,"
-    pk_header += "peak start/ min,"
-    pk_header += "peak end/ min\n"
-
-    unit = peak_collection.series_unit
-    value = peak_collection.series_value
     with open(fname, "w") as f:
-        f.write(f"{unit},{value}\n")
-        f.write(IS_header)
+        f.write(text)
 
-        IS_rt = peak_collection.internal_standard.retention_time
-        IS_integ = peak_collection.internal_standard.integral
-        strt = peak_collection.internal_standard.start
-        end = peak_collection.internal_standard.end
-
-        f.write(f"{IS_rt},{IS_integ},{strt},{end}\n")
-        f.write(pk_header)
-        for p in peak_collection.peaks:
-            rt = p.retention_time
-            integ = p.integral
-            start = p.start
-            end = p.end
-            f.write(f"{rt},{integ},{start},{end}\n")
