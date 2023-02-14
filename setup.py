7c9247ca2dd82d580bc1eb7b9b3d4fc6b828bcf6
Will <werobinson6@gmail.com>
2023-02-14 11:03:36 +0100

    update dependencies and version

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name="ChromProcess",
-    vesion="0.0.0",
+    vesion="0.0.1",
     author=["William E. Robinson"],
     packages=["ChromProcess"],
     install_requires=[
-        "numpy >= 1.21.1",
-        "scipy >= 1.7.3",
-        "netcdf4 >= 1.5.7",
+        "numpy >= 1.23.5",
+        "scipy >= 1.9.3",
+        "netcdf4 >= 1.6.0",
         "tomli >= 2.0.1",
     ],
     extras_require={"dev": ["pdoc", "black"]},
