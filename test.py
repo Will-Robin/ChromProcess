e38126c87a263c6617afeaac63143fd838bec2b0
Will <werobinson6@gmail.com>
2023-06-11 11:36:48 +0200

    using a Python list seems to be faster than numpy

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 import time
+import timeit
 from ChromProcess import chromate
 from ChromProcess.Loading import chrom_from_csv
 from ChromProcess.Utils.signal_processing import signal_processing as sig
+from scipy.signal import find_peaks, peak_widths
 import matplotlib.pyplot as plt
 
 file = "Tutorials/example_data/Example/ExperimentalData/ChromatogramCSV/chrom_001.csv"
@@ -11,7 +13,15 @@ chrom = chrom_from_csv(file)
 smoothed = sig.adjacent_average(chrom.signal, 10)
 
 start = time.time()
-peaks, starts, ends = chromate.find_peaks(smoothed, chrom.signal.max()*0.1)
+peaks, _ = find_peaks(smoothed, height=chrom.signal.max()*0.1)
+results_half = peak_widths(smoothed, peaks, rel_height=0.5)
+end = time.time()
+print(f"Scipy benchmark {(end-start)*1000} ms")
+
+print("next")
+sig_list = smoothed.tolist()
+start = time.time()
+peaks, starts, ends = chromate.find_peaks(sig_list, chrom.signal.max()*0.1)
 end = time.time()
 print(f"Took {(end-start)*1000} ms")
 
